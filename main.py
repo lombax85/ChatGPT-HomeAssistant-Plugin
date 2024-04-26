a53707916a7429aa25c3fa394caa0b7ac700989a
Fabio Lombardo - Walatesting <lombax85@gmail.com>
2024-04-26 13:36:26 +0000

    Added bearer token support

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
-from config import headers, home_assistant_url
+from config import headers, home_assistant_url, bearer_token
 
 import json
 
 import quart
 import quart_cors
 import requests
-from quart import request
+from quart import request, abort
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
 # Keep track of todo's. Does not persist if Python session is restarted.
 _TODOS = {}
 
+def verify_bearer_token(request):
+    auth_header = request.headers.get('Authorization')
+    print(auth_header)
+    if not auth_header or auth_header.split(" ")[0].lower() != "bearer" or auth_header.split(" ")[1] != bearer_token:
+        abort(401, "Invalid Token")
 
 def getLightStatus():
+    verify_bearer_token(request)
     url = f"{home_assistant_url}/api/states"
     response = requests.get(url, headers=headers)
     data = response.json()
 
-    valid_entities = ['light', 'switch']
+    valid_entities = ['light', 'switch', 'sensor']
 
-    lights_switches_data = [{"name": entity["attributes"]["friendly_name"],
+    lights_switches_data = [{"name": entity["attributes"].get("friendly_name", entity["entity_id"]),
                             "id": entity["entity_id"],
                             "status": entity["state"]}
                             for entity in data if entity["entity_id"].split(".")[0] in valid_entities]
@@ -28,6 +34,7 @@ def getLightStatus():
     return lights_switches_data
 
 def setLightStatus(type: str, light_id: str, light_status: str):
+    verify_bearer_token(request)
     url = f"{home_assistant_url}/api/services/{type}/turn_{light_status}"
 
     payload = {"entity_id": light_id}
@@ -46,10 +53,12 @@ def setLightStatus(type: str, light_id: str, light_status: str):
 
 @app.get("/lights/status")
 async def get_lights_status():
+    verify_bearer_token(request)
     return quart.Response(response=json.dumps(getLightStatus()), status=200)
 
 @app.post("/set/<string:type>/<string:id>/<string:status>")
 async def set_light_status(type, id, status):
+    verify_bearer_token(request)
     res = setLightStatus(type, id, status)
     if res:
         return "ok"
